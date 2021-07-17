0c3d7af53e93ef2814762f06b61087784671263f
LucassMota <77123260+LucassMota@users.noreply.github.com>
2021-07-17 20:29:30 -0300

    Update pomodoro.js

--- src/timer/pomodoro/pomodoro.js
+++ src/timer/pomodoro/pomodoro.js
@@ -3,6 +3,11 @@ import * as workerTimers from 'worker-timers'
 let startTimer
 let running = false
 
+let ws = '60'
+let wm = '25'
+
+let cycles = 0
+
 const buttonRestart = document.createElement('button')
 buttonRestart.setAttribute('id', 'btn-restart')
 buttonRestart.setAttribute('class', 'btn-start')
@@ -16,11 +21,17 @@ buttonPause.innerHTML = 'Começar Pausa'
 function startStudy() {
   // workerTimers.clearInterval(startTimer)
 
+  if (cycles <= 4) {
+    cycles++
+  } else cycles = 0
+
+  console.log(cycles)
+
+  wm = 24
+
   if (!running) {
     startTimer = workerTimers.setInterval(timerPomodoro, 1000)
   }
-
-  wm = 24
 
   document.getElementById('start').remove()
 
@@ -29,6 +40,7 @@ function startStudy() {
 
 buttonPause.onclick = () => {
   if (
+    wm > 0 &&
     !window.confirm(
       'O tempo de estudo ainda não acabou. Tem certeza que gostaria de iniciar a pausa agora?'
     )
@@ -36,28 +48,43 @@ buttonPause.onclick = () => {
     return
   } else {
     workerTimers.clearInterval(startTimer)
-    ws = 60
-    wm = 4
 
-    document.getElementById('w_minutes').innerText = '05'
-    document.getElementById('w_seconds').innerText = '00'
+    if (cycles === 4) {
+      ws = 60
+      wm = 14
+
+      document.getElementById('w_minutes').innerText = '15'
+      document.getElementById('w_seconds').innerText = '00'
+    } else {
+      ws = 60
+      wm = 4
+
+      document.getElementById('w_minutes').innerText = '05'
+      document.getElementById('w_seconds').innerText = '00'
+    }
 
     startTimer = workerTimers.setInterval(timerPomodoro, 1000)
 
     document.getElementById('btn-stop').remove()
     document.getElementById('container').appendChild(buttonRestart)
   }
-  // }
 }
 
 buttonRestart.onclick = () => {
   if (
+    wm > 0 &&
     !window.confirm(
       'O tempo de pausa ainda não acabou. Tem certeza que gostaria de retomar os estudos agora?'
     )
   ) {
     return
   } else {
+    if (cycles <= 4) {
+      cycles++
+    } else cycles = 0
+
+    console.log(cycles)
+
     workerTimers.clearInterval(startTimer)
     ws = 60
     wm = 24
@@ -70,23 +97,22 @@ buttonRestart.onclick = () => {
     document.getElementById('btn-restart').remove()
     document.getElementById('container').appendChild(buttonPause)
   }
-  // }
 }
 
-let ws = '60'
-let wm = '25'
-
 function timerPomodoro() {
-  if (wm > 0) {
-    ws--
+  if (wm >= 0) {
+    if (wm == 0 && ws == '00') {
+      return
+    } else ws--
 
-    if (ws < 1) {
-      ws = '00'
-      wm--
+    if (ws < 0) {
+      ws = 59
+      if (wm > 0) {
+        wm--
+      } else return
     }
 
     if (wm !== 0 && ws === '00') {
-      ws = '00'
       ws = 59
     }
 
@@ -100,7 +126,7 @@ function timerPomodoro() {
     if (wm >= 10) {
       document.getElementById('w_minutes').innerText = wm
     } else document.getElementById('w_minutes').innerText = '0' + wm
-  } else return
+  } else if (wm === 0 && ws === 0) return
 }
 
 const pomodoro = { startStudy }
