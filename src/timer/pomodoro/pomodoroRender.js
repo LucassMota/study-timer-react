5fd54c62eed59c2d8a4a4652622223890053ab37
LucassMota <77123260+LucassMota@users.noreply.github.com>
2021-07-17 21:28:26 -0300

    Update pomodoroRender.js

--- src/timer/pomodoro/pomodoroRender.js
+++ src/timer/pomodoro/pomodoroRender.js
@@ -5,7 +5,7 @@ import pomodoro from './pomodoro'
 import { Helmet } from 'react-helmet'
 
 export default function PomodoroRender() {
-  // console.clear()
+  console.clear()
   return (
     <div>
       <Helmet>
@@ -17,21 +17,19 @@ export default function PomodoroRender() {
         </style>
       </Helmet>
       <h1 className='pomodoro-title'>Pomodoro</h1>
-      <div className='container'>
-        <div id='container'>
-          <div className='timer-pomodoro'>
-            <h1 id='w_minutes'>25</h1>
-            <h1 id='semicolon'>:</h1>
-            <h1 id='w_seconds'>00</h1>
-          </div>
-          <button
-            id='start'
-            className='btn-start'
-            onClick={() => pomodoro.startStudy()}
-          >
-            Começar!
-          </button>
+      <div id='container-pomodoro' className='container-pomodoro'>
+        <div className='timer-pomodoro'>
+          <h1 id='w_minutes'>25</h1>
+          <h1 id='semicolon'>:</h1>
+          <h1 id='w_seconds'>00</h1>
         </div>
+        <button
+          id='start'
+          className='btn-start'
+          onClick={() => pomodoro.startStudy()}
+        >
+          Começar!
+        </button>
       </div>
     </div>
   )
